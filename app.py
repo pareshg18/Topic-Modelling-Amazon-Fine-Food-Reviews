9854d192144d557dc831de4906ba69f0e11ee7dc
Paresh Gupta <45079009+pareshg18@users.noreply.github.com>
2020-07-08 14:53:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,15 @@ with open('stop_words_amazon', 'rb') as f:
 with open('lda_dictionary', 'rb') as f:
     dct_lda = pickle.load(f)
 
+file2 = 'topic_distribution.pkl'
+topic_distribution = pickle.load(open(file2, 'rb'))
 
-# In[ ]:
+file3 = 'reviews_original.pkl'
+reviews = pickle.load(open(file3, 'rb'))
+    
+
+file4 = 'indexes.pkl'
+indexes = pickle.load(open(file4, 'rb'))
 
 
 #cv=pickle.load(open('tranform.pkl','rb'))
@@ -60,8 +67,8 @@ def predict():
     
     
     lda_theme = {0:'Delivery/ Order Related Issues',1:'Breakfast Food',2:'Chewing Gums/ Dog Treats',3:'Bad Products',4:'Energy Drinks/ Sports Drinks',5:'Packaging Bags',
-             6:'Sweet Snacks(Chocolates)',7:'Beverages (Juices)',8:'Gifts/High Calorie Foods', 9:'Spicy Food/ Soups',10:'A Baking Recipe',11:'Pet Issues',
-             12:'Tea(s)',13:'Salty Snacks(Crackers)', 14:'Hair Products/ Cat Issues', 15: 'Coffee'}
+             6:'Sweet Snacks (like Chocolates?)',7:'Beverages (Juices)',8:'Gifts/High Calorie Foods', 9:'Spicy Food/ Soups',10:'A Baking Recipe',11:'Pet Issues',
+             12:'Tea(s)',13:'Salty Snacks (lie Crackers?)', 14:'Hair Products/ Cat Issues', 15: 'Coffee'}
     
     #nlp = spacy.load('en_core_web_sm')
 
@@ -89,16 +96,29 @@ def predict():
         document = clean_text(document)        
         doc_vector_lda = dct_lda.doc2bow(document.split(" "))
         topic1,perc1 = sorted(lda[doc_vector_lda],key = lambda x:x[1],reverse = True)[0]
-        return lda_theme[topic1]
-        #print("LDA predicts this document to be talking about",'\x1b[1;31m'+ lda_theme[topic1]  +'\x1b[0m')
-        
+        no_docs = topic_distribution.total_documents[topic_distribution.topic_no == topic1]
+        temp = no_docs.at[topic1]
+        return [lda_theme[topic1],temp,topic1]
+    
+    def get_reviews(topic):
+        temp = []
+        for j in range(3):
+            temp.append(reviews.review[reviews.indexing == indexes[topic][1][j]].values[0])
+        return temp
+
+              
     if request.method == 'POST':
         message = request.form['message']
+        if len(message) == 0:
+            return render_template('home.html',prediction_text1 = 'Please provide an input')        
+        
         data = message
-        #vect = cv.transform(data).toarray()
-        my_prediction = predict_topic(data)
-    return render_template('home.html',prediction_text = 'Your wrote a review about ' + my_prediction)
-
+        
+        output = predict_topic(data)
+        rev = get_reviews(output[2])
+        pred1 = output[0]
+        pred2 = output[1]
+    return render_template('home.html',prediction_text1 = 'Your review is about ' + pred1, prediction_text2 = 'There are a total of ' + "{:,.0f}".format(pred2) + ' reviews available on ' + pred1 + '. Here are the top 3 reviews closest to your review:',prediction_text3 = '1. ' + rev[0],prediction_text4 = '2. ' + rev[1],prediction_text5 = '3. ' + rev[2])
 
 
 if __name__ == '__main__':
